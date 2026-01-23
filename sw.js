cac630aae6e9392e6badd774a39740fb8a9f08f8
surpraz4-maker <surpra.z4@gmail.com>
2026-01-23 14:56:32 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+"name": "YAKUtime Maps",
+"short_name": "YAKUtime",
+"start_url": "./index.html",
+"display": "standalone",
+"background_color": "#ffffff",
+"theme_color": "#2563eb",
+"icons": [
+{
+"src": "https://www.google.com/search?q=https://cdn-icons-png.flaticon.com/512/854/854878.png",
+"sizes": "512x512",
+"type": "image/png"
+}
+]
+}
